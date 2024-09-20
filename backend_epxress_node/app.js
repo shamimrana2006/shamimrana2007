426d99708717dba79724106fac1c7617a25d3e02
shamimrana2006 <mdshamimrana20@gmail.com>
2024-09-20 14:40:06 +0600

    finaly succes using error handler with only on router not found 404j

--- backend_epxress_node/app.js
+++ backend_epxress_node/app.js
@@ -1,8 +1,23 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("shamim rana it's work fine")
-})
+app.get("/aaa", (req, res) => {
+  res.send("shamim rana it's work fine");
+});
+app.get("/", (req, res) => {
+  res.send("shamim rana it's work fine");
+});
 
-module.exports =app
+app.use((req, res, next) => {
+  const error = new Error("router not found ");
+  error.statusCode = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  console.log(err.statusCode);
+
+  res.send(`server is error status code is and message is ${err.message} `);
+});
+
+module.exports = app;
